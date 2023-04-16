1454526b3951d2bc1d10e0fdef914fbcb00c8273
afsarjahan ansari <nagma1804@gmail.com>
2023-04-16 15:41:11 +0530

    changing requence of education

--- src/pages/Qualification/Qualification-tabs/Education.jsx
+++ src/pages/Qualification/Qualification-tabs/Education.jsx
@@ -7,12 +7,11 @@ const Education = () => {
         <div className="education__container">
             <div className="timeline_container">
                 <div className="education-item">
-                    <h5>Secondary School</h5>
-                    <p className='education-title'>Kalwa Hindi High School</p>
-                    <p className='education-title'><GoCalendar /> 2016-2017</p>
+                    <h5>Grduation</h5>
+                    <p className='education-title'>Sahyog college of Management studies</p>
+                    <p className='education-title'><GoCalendar /> 2020-2023</p>
                 </div>
             </div>
-
             <div className="timeline_container">
                 <div className="education-item">
                     <h5>High School</h5>
@@ -20,15 +19,13 @@ const Education = () => {
                     <p className='education-title'><GoCalendar /> 2017-2020</p>
                 </div>
             </div>
-
             <div className="timeline_container">
                 <div className="education-item">
-                    <h5>Grduation</h5>
-                    <p className='education-title'>Sahyog college of Management studies</p>
-                    <p className='education-title'><GoCalendar /> 2020-2023</p>
+                    <h5>Secondary School</h5>
+                    <p className='education-title'>Kalwa Hindi High School</p>
+                    <p className='education-title'><GoCalendar /> 2016-2017</p>
                 </div>
             </div>
-
         </div>
     )
 }
